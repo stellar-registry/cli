e2cd6cab4211e37228d47f674d3a3dbd450863ad
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2026-07-30 13:57:20 -0400

    build: test fetch_contract_id default fails for flagged (#60)
    Add test demonstrating/proving that `fetch_contract_id` fails for
    flagged contracts, and allows overriding with `--force`
    
    `import_contract!` uses `fetch_contract_id` internally, so this also
    proves that `import_contract!` fails a build that relies on a flagged
    contract.

--- crates/stellar-registry-cli/src/commands/fetch_contract_id.rs
+++ crates/stellar-registry-cli/src/commands/fetch_contract_id.rs
@@ -63,7 +63,7 @@ impl Cmd {
 #[cfg(feature = "integration-tests")]
 #[cfg(test)]
 mod tests {
-    use stellar_registry_test::RegistryTest;
+    use stellar_registry_test::{AssertExt, RegistryTest};
 
     #[tokio::test]
     async fn simple() {
@@ -139,5 +139,71 @@ mod tests {
             .await
             .unwrap();
         assert!(!contract_id.to_string().is_empty());
+    }
+
+    #[tokio::test]
+    async fn flagged() {
+        let registry = RegistryTest::new().await;
+        let v1 = registry.hello_wasm_v1();
+
+        // 1. publish wasm
+        registry
+            .registry_cli("publish")
+            .arg("--wasm")
+            .arg(v1.to_str().unwrap())
+            .arg("--binver")
+            .arg("0.0.1")
+            .arg("--wasm-name")
+            .arg("hello")
+            .assert()
+            .success();
+
+        // 2. deploy contract
+        let output = registry
+            .registry_cli("deploy")
+            .arg("--contract-name")
+            .arg("hello")
+            .arg("--wasm-name")
+            .arg("hello")
+            .arg("--")
+            .arg("--admin=alice")
+            .assert()
+            .success()
+            .stdout_as_str();
+        let flagged_id = output.split_whitespace().last().unwrap();
+
+        // 3. Flag contract
+        registry
+            .env
+            .stellar("contract")
+            .args([
+                "invoke",
+                "--id",
+                &registry.registry_address,
+                "--",
+                "flag_contract",
+                "--flagged",
+                "true",
+                "--contract-name",
+                "hello",
+            ])
+            .assert()
+            .success();
+
+        let err = registry
+            .parse_cmd::<super::Cmd>(&["hello"])
+            .unwrap()
+            .fetch_contract_id()
+            .await
+            .unwrap_err();
+        assert!(err.to_string().contains("flagged"));
+
+        let contract_id = registry
+            .parse_cmd::<super::Cmd>(&["hello", "--force"])
+            .unwrap()
+            .fetch_contract_id()
+            .await
+            .unwrap();
+        assert_eq!(*contract_id.to_string(), *flagged_id);
     }
 }
